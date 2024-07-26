ce68b6a217006cab7cf5fdac96bf9da39e3d93a7
Bence Balogh <benceeok@gmail.com>
2024-07-26 20:53:38 +0200

    updated config

--- source/conf.py
+++ source/conf.py
@@ -14,11 +14,13 @@ release = "0.0.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinxcontrib.bibtex"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
 
+# Make sure the target is unique
+autosectionlabel_prefix_document = True
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
@@ -38,7 +40,11 @@ html_theme_options = {
     "use_edit_page_button": False,
     "logo": {
         "text": "Home",
-    }
+    },
+    "header_links_before_dropdown": 6,
 }
 
+html_sidebars = {"**": []}
 
+# -- Options for bibtex ----------------------------------------
+bibtex_bibfiles = ["refs.bib"]
